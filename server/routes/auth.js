75782d4de05a8bac594eac28e39a72c12a020675
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2020-03-10 19:57:22 -0300

    login y registro creado

---
+++ server/routes/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const { createUser,login } = require('../services/authService');
+
+
+app.post('/signup', async (req, res) => {
+    try {
+        let body = req.body;
+        let query = await createUser(body.username, body.email, body.password);
+        return res.status(200).json(query);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    try{
+        let body = req.body;
+        let query = await login(body.email,body.password);
+        return res.status(200).json(query);
+    }catch(error){
+        return res.status(400).json(error);
+    }
+});
+
+
+module.exports = app;
